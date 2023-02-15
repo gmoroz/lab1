f73bc684237bb7273d2d96a223826882b030fb8e
gmoroz <12345hasek@gmail.com>
2023-02-15 16:24:11 +0300

    refactor: query for games

--- main/views.py
+++ main/views.py
@@ -1,9 +1,8 @@
 from typing import Any
 
-from django.db.models import OuterRef, Subquery
 from django.views.generic import TemplateView
 
-from .models import Calendar, Team
+from .models import Calendar
 
 
 class GamesView(TemplateView):
@@ -11,27 +10,10 @@ class GamesView(TemplateView):
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        subquery_team1 = Subquery(
-            Team.objects.filter(
-                calendar__pk=OuterRef("pk"),
-            ).values("name")[:1],
+
+        games_qs = Calendar.objects.prefetch_related("teams").order_by(
+            "date_of_the_match",
         )
-        subquery_team2 = Subquery(
-            Team.objects.filter(
-                calendar__pk=OuterRef("pk"),
-            ).values("name")[1:2],
-        )
-        context["games"] = (
-            Calendar.objects.annotate(
-                team1_name=subquery_team1, team2_name=subquery_team2,
-            )
-            .values(
-                "date_of_the_match",
-                "team1_name",
-                "team2_name",
-                "main_judge",
-                "result",
-            )
-            .order_by("date_of_the_match")
-        )
+
+        context["games"] = games_qs
         return context
