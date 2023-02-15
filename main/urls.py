9884d569c16ea6c25e5651d9b553fa37d50f34e9
gmoroz <12345hasek@gmail.com>
2023-02-16 02:04:43 +0300

    feat: add form for create team

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import GamesView, TeamViewSet, TitleViewSet, TournamentView
+from .views import (
+    GamesView,
+    TeamFormViewForCreate,
+    TeamViewSet,
+    TitleViewSet,
+    TournamentView,
+)
 
 router = SimpleRouter()
 router.register("teams", TeamViewSet, basename="teams")
@@ -11,6 +17,11 @@ router.register("titles", TitleViewSet, basename="titles")
 urlpatterns = [
     path("games/", GamesView.as_view(), name="games"),
     path("table/", TournamentView.as_view(), name="table"),
+    path(
+        "teams/create/",
+        TeamFormViewForCreate.as_view(),
+        name="teams-create",
+    ),
 ]
 
 urlpatterns += router.urls
