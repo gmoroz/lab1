f235ebba393a3cec42ce3c0cdef8d0dfc071d06d
gmoroz <12345hasek@gmail.com>
2023-02-16 00:34:35 +0300

    feat: add static load

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import GamesView, TeamViewSet, TournamentView
+from .views import GamesView, TeamViewSet, TitulViewSet, TournamentView
 
 router = SimpleRouter()
 router.register("teams", TeamViewSet, basename="teams")
+router.register("tituls", TitulViewSet, basename="tituls")
 
 
 urlpatterns = [
