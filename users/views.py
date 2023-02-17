52ebf876637fd5e8ece1e2f98c69a29ee24e524c
gmoroz <12345hasek@gmail.com>
2023-02-17 18:33:26 +0300

    fix: finish with login and register views

--- users/views.py
+++ users/views.py
@@ -1,40 +1,45 @@
-from django.contrib.auth import authenticate, login
-from django.urls import reverse_lazy
-from django.views.generic import FormView
-
-from .forms import LoginForm, RegistrationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 
 
-class LoginView(FormView):
-    form_class = LoginForm
-    template_name = "auth.html"
-    success_url = reverse_lazy("main")
-
-    def form_valid(self, form):
-        username = form.cleaned_data["username"]
-        password = form.cleaned_data["password"]
-        user = authenticate(self.request, username=username, password=password)
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(self.request, user)
-            return super().form_valid(form)
+            login(request, user)
+            return redirect("home")
         else:
-            form.add_error(None, "Invalid username or password")
-            return super().form_invalid(form)
+            return render(
+                request, "login.html", {
+                    "error": "Неправильный логин или пароль.",
+                },
+            )
+    else:
+        return render(request, "login.html")
 
 
-class RegisterView(FormView):
-    form_class = RegistrationForm
-    template_name = "auth.html"
-    success_url = reverse_lazy("main")
+def logout_view(request):
+    logout(request)
+    return redirect("login")
 
-    def form_valid(self, form):
-        form.save()
-        username = form.cleaned_data["username"]
-        password = form.cleaned_data["password1"]
-        user = authenticate(self.request, username=username, password=password)
-        if user is not None:
-            login(self.request, user)
-            return super().form_valid(form)
-        else:
-            form.add_error(None, "Invalid username or password")
-            return super().form_invalid(form)
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+
+        if password != confirm_password:
+            error = "Пароли не совпадают"
+            return render(request, "register.html", {"error": error})
+
+        user = User.objects.create_user(username, password=password)
+
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect("main")
+
+    return render(request, "register.html")
