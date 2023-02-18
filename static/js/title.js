e9920c7dbc60804fc1673174460f60a3777b52a0
gmoroz <12345hasek@gmail.com>
2023-02-18 21:57:22 +0300

    refactor: updated CSS styles to improve design and functionality
    Changed the CSS styles in the following ways:
    
        Renamed ".container" to ".title-container"
        Adjusted styling for ".title-container", "h1", and "form"
        Updated label and input styles
        Added a new class, ".save-btn", for the save button
        Added new JavaScript code to handle form submission with a PUT request and improved error handling

--- static/js/title.js
+++ static/js/title.js
@@ -1,19 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var form = document.querySelector("#title-form");
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    var titleId = document.querySelector("#title-id").value;
-    var name = document.querySelector("#name").value;
-    var xhr = new XMLHttpRequest();
-    xhr.open("PUT", "/titles/" + titleId + "/");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        location.reload();
-      } else {
-        alert("Unable to update title.");
+// Получаем форму и кнопку сохранения
+const titleForm = document.getElementById("title-form");
+const saveBtn = document.getElementById("save-btn");
+
+// Обработчик события нажатия на кнопку сохранения
+saveBtn.addEventListener("click", function (e) {
+  e.preventDefault(); // Отменяем стандартное действие кнопки
+
+  // Получаем данные формы
+  const titleId = document.getElementById("title-id").value;
+  const titleName = document.getElementById("name").value;
+
+  // Отправляем PUT-запрос на сервер
+  fetch(`/titles/${titleId}/`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": "{{ csrf_token }}", // Передаем CSRF-токен для защиты от CSRF-атак
+    },
+    body: JSON.stringify({ name: titleName }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
-    };
-    xhr.send(JSON.stringify({ name: name }));
-  });
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Data saved:", data);
+      // Дополнительные действия после успешного сохранения
+    })
+    .catch((error) => {
+      console.error("Error saving data:", error);
+      // Дополнительные действия при ошибке сохранения
+    });
 });
