ad6b36d551cfce39d364f560a1f89dc5823e9cac
gmoroz <12345hasek@gmail.com>
2023-02-19 01:28:55 +0300

    refactor: team form submission handling and update team page template.
    Replace the previous form submission handling code with jQuery's $.ajax method, which sends an AJAX PUT request to the server. This approach is more flexible and allows us to customize the request headers and handle the server's response more easily.
    
    In addition, modify the team page template to add the {% csrf_token %} template tag to the form, which is required to prevent Cross-Site Request Forgery (CSRF) attacks. Also, remove the script tag that loads the team.js file, which is no longer needed.

--- static/js/team.js
+++ static/js/team.js
@@ -1,16 +1,24 @@
-window.onload = function () {
-  const form = document.getElementById("team-form");
-  const submitButton = form.querySelector('input[type="submit"]');
-  submitButton.addEventListener("click", function (event) {
+$(document).ready(function () {
+  const teamId = window.location.pathname.split("/").filter(Boolean).pop();
+
+  $("#team-form").on("submit", function (event) {
     event.preventDefault();
-    const formData = new FormData(form);
-    const teamId = window.location.pathname.split("/").filter(Boolean).pop();
-    fetch(`/teams/${teamId}/`, {
-      method: "PUT",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
+    const formData = $(this).serialize();
+
+    $.ajax({
+      type: "PUT",
+      url: `/teams/${teamId}/`,
+      data: formData,
+      beforeSend: function (xhr, settings) {
+        const csrftoken = $('input[name="csrfmiddlewaretoken"]').val();
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      },
+      success: function (response) {
+        alert("Команда успешно сохранена!");
+      },
+      error: function (response) {
+        alert("Произошла ошибка при сохранении команды");
+      },
+    });
   });
-};
+});
