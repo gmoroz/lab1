28630e420d26f39ed2f7af4691a244eaf065d3a4
gmoroz <12345hasek@gmail.com>
2023-02-19 03:49:28 +0300

    fix: timeout for slideDown

--- static/js/base.js
+++ static/js/base.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   $(".nav-item").hover(
     function () {
-      $(this).find("ul").stop(true, true).slideDown(200);
+      $(this).find("ul").stop(true, true).slideDown(400);
     },
     function () {
       setTimeout(() => {
-        $(this).find("ul").stop(true, true).slideUp(200);
+        $(this).find("ul").stop(true, true).slideUp(400);
       }, 100);
     }
   );
