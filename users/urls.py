b85881239aff618ee6e6c168617ec0599b315308
gmoroz <12345hasek@gmail.com>
2023-02-19 07:28:05 +0300

    perf: move websocket urls to routers.py

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 
-from .consumers import ChatConsumer
 from .views import login_view, logout_view, register_view
 
 urlpatterns = [
     path("login/", login_view, name="login"),
     path("register/", register_view, name="register"),
     path("logout/", logout_view, name="logout"),
-]
-
-websocket_urlpatterns = [
-    path("ws/chat/<str:room_name>/", ChatConsumer.as_asgi()),
 ]
