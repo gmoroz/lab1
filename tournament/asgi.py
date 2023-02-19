e3a8408b9f82cf78155a6813fc57952c2e6a75ae
gmoroz <12345hasek@gmail.com>
2023-02-19 07:30:38 +0300

    fix: some simple mistakes

--- tournament/asgi.py
+++ tournament/asgi.py
@@ -13,11 +13,11 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from users.urls import websocket_urlpatterns
+from users.routets import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tournament.settings")
 
-application = application = ProtocolTypeRouter(
+application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
