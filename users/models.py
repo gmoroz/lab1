e57e2c8cf69125cedd4d2c6ece1e455af772b841
gmoroz <12345hasek@gmail.com>
2023-02-19 11:58:34 +0300

    feat: add ChatMessage model

--- users/models.py
+++ users/models.py
@@ -1,2 +1,9 @@
+from django.contrib.auth.models import User
+from django.db import models
 
-# Create your models here.
+
+class ChatMessage(models.Model):
+    room = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
