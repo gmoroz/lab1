a910e21171a99fa3764e3eca37f78c393f89cdb4
gmoroz <12345hasek@gmail.com>
2023-02-20 16:52:37 +0300

    feat: Add docker-compose and update ASGI configuration
    Added docker-compose.yaml with services for web, db, and redis_db
    Updated ASGI configuration in asgi.py to fix "ImproperlyConfigured" error
    Updated CHANNEL_LAYERS in settings.py to use environment variable for Redis host

--- tournament/asgi.py
+++ tournament/asgi.py
@@ -1,13 +1,15 @@
 import os
 
 import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tournament.settings")
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
 from users.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tournament.settings")
 django_asgi_app = get_asgi_application()
 
 
@@ -17,10 +19,3 @@ application = ProtocolTypeRouter(
         "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     },
 )
-
-
-def load_django():
-    django.setup()
-
-
-worker_preload = load_django
