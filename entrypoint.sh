ed9155955460a808741b26aa3358fed447cf12c7
gmoroz <12345hasek@gmail.com>
2023-02-20 12:29:38 +0300

    feat: Add Dockerfile, entrypoint.sh and dockerignore
    Add Dockerfile with Python 3.10-alpine image and install dependencies using Poetry. Create entrypoint.sh script to run migrations, load data fixtures and start Django server. Add dockerignore file to exclude unnecessary files from the Docker build.

---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+python manage.py migrate
+python manage.py loaddata main/fixtures/calendar.json
+python manage.py loaddata main/fixtures/teams.json
+python manage.py loaddata main/fixtures/titles.json
+python manage.py runserver 0.0.0.0:80
