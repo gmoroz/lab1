fc73c63305b371db3c40ea1da6b092afb4964b08
gmoroz <12345hasek@gmail.com>
2023-02-20 18:53:54 +0300

    feat: Add chat selection page and corresponding CSS.
    This commit adds a new page for selecting a chat room, along with the necessary HTML and CSS files. The page allows users to enter the name of the chat room they want to join and then redirects them to the corresponding chat page. The CSS file defines the styling for the chat selection form, including font sizes, colors, and layout.

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import GetRoomMessages, RoomView, login_view, logout_view, register_view
 
@@ -8,4 +9,9 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("chat/<str:room_name>/", RoomView.as_view()),
     path("messages/<str:room>/", GetRoomMessages.as_view()),
+    path(
+        "chat/",
+        TemplateView.as_view(template_name="select_chat.html"),
+        name="chat",
+    ),
 ]
