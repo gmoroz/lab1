6c40cdee26b488ce46f0f113924401ad7e75e7dd
gmoroz <12345hasek@gmail.com>
2023-02-20 23:43:17 +0300

    feat: Add nginx service and change static file location
    This commit adds a new nginx service to the docker-compose file and changes the location of the static files volume to /usr/src/app/static_files. The nginx service is added to allow serving static files with better performance, and the change in volume location is necessary for nginx to be able to serve the static files. Also, the urls.py file is updated to use the serve() function to serve the static files.

--- tournament/urls.py
+++ tournament/urls.py
@@ -14,18 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
+from django.views.static import serve
 
 urlpatterns = [
     path(
-        "", login_required(TemplateView.as_view(template_name="main.html")), name="main",
+        "",
+        login_required(TemplateView.as_view(template_name="main.html")),
+        name="main",
     ),
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path("", include("users.urls")),
+    re_path(
+        r"^static/(?P<path>.*)$", serve,
+        {"document_root": settings.STATIC_ROOT},
+    ),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
