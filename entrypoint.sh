b7d16b00f9d96d6ca389d084c93a36c1e553f301
gmoroz <12345hasek@gmail.com>
2023-02-20 16:53:01 +0300

    fix: Replace Daphne with Uvicorn in entrypoint.sh
    Replaced the Daphne ASGI server with the Uvicorn ASGI server in the entrypoint.sh script.
    Updated the command to start the server for use uvicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ python manage.py migrate
 python manage.py loaddata main/fixtures/calendar.json
 python manage.py loaddata main/fixtures/teams.json
 python manage.py loaddata main/fixtures/titles.json
-python manage.py runserver 0.0.0.0:80
+uvicorn tournament.asgi:application --host 0.0.0.0 --port 80
