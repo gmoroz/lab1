0b3eed619bf63697755aac6c22bcc1e71e94812a
gmoroz <12345hasek@gmail.com>
2023-02-21 21:40:22 +0300

    feat: Add validation and error messages to game update form
    The code changes include adding validation for form input fields, such as checking that the team names are different, that the date of the match is not in the future if a result is provided, and that the main judge name only contains letters and spaces. Error messages are displayed to the user if the input data does not meet these requirements.

--- static/js/game.js
+++ static/js/game.js
@@ -8,7 +8,40 @@ const resultField = $("#result");
 const gameId = window.location.pathname.split("/").filter(Boolean).pop();
 
 form.submit(function (event) {
+  // Отменяем действие по умолчанию браузера
   event.preventDefault();
+  // Получаем данные из полей формы
+  const date = dateField.val();
+  const team1 = team1Field.val();
+  const team2 = team2Field.val();
+  const judge = judgeField.val();
+  const result = resultField.val();
+  // Проверяем, что команды не одинаковые
+  if (team1 === team2) {
+    alert("Нельзя выбрать две одинаковые команды.");
+    return;
+  }
+
+  // Проверяем, что дата не в будущем и если так, то результат должен быть пустым
+  const today = new Date().toISOString().slice(0, 10);
+  if (date > today && result !== "") {
+    alert("Нельзя поставить результат для матча в будущем!");
+    return;
+  }
+  // Проверяем, что результат задан в формате "число-число"
+  const regexResult = /^\d+-\d+$/;
+  if (result !== "" && !regexResult.test(result)) {
+    alert('Результат должен быть в формате "число-число".');
+    return;
+  }
+
+  // Проверяем, что в имени судьи только буквы и пробелы
+  const regexJudge = /^[a-zA-Zа-яА-Я ]+$/;
+  if (!regexJudge.test(judge)) {
+    alert("В имени судьи должны быть только буквы.");
+    return;
+  }
+
   const data = {
     date_of_the_match: dateField.val(),
     teams: [team1Field.val(), team2Field.val()],
@@ -26,10 +59,10 @@ form.submit(function (event) {
   })
     .done(function (data) {
       console.log("Success:", data);
-      alert("Game updated successfully!");
+      alert("Игра обновлена успешно!");
     })
     .fail(function (jqXHR, textStatus, errorThrown) {
       console.error("Error:", textStatus, errorThrown);
-      alert("An error occurred while updating the game.");
+      alert("Произошла ошибка при создании матча.");
     });
 });
