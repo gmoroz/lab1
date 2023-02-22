960e99aeed686619c041af78f83a8547100486f2
gmoroz <12345hasek@gmail.com>
2023-02-22 17:53:32 +0300

    feat: Added client-side form validation using jQuery for team creation page
    Added jQuery script to validate the input fields on the team creation page. The script checks that the team name, country, city, coach, points, and games count fields only contain valid characters and values. If any of the fields are invalid, an alert message is displayed. This improves the user experience and helps prevent invalid data from being submitted to the server.

---
+++ static/js/team_create.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  $("#team-form").submit(function (event) {
+    var name = $("#name").val();
+    var country = $("#country").val();
+    var city = $("#city").val();
+    var coach = $("#coach").val();
+    var points = $("#points").val();
+    var gamesCount = $("#games-count").val();
+
+    var regexLetters = /^[a-zA-Zа-яА-ЯёЁ ]+$/;
+    var regexNumbers = /^[0-9]+$/;
+
+    if (!regexLetters.test(name)) {
+      alert("В названии команды могут быть только буквы!");
+      event.preventDefault();
+    }
+    if (!regexLetters.test(country)) {
+      alert("В названии страны могут быть только буквы!");
+      event.preventDefault();
+    }
+    if (!regexLetters.test(city)) {
+      alert("В названии города могут быть только буквы!");
+      event.preventDefault();
+    }
+    if (!regexLetters.test(coach)) {
+      alert("В имени тренера могут быть только буквы!");
+      event.preventDefault();
+    }
+    if (!regexNumbers.test(points) || points < 0) {
+      alert("Очки могут быть только положительными целыми числами или нулем!");
+      event.preventDefault();
+    }
+    if (!regexNumbers.test(gamesCount) || gamesCount < 0) {
+      alert(
+        "Количество игр может быть только положительным целым числом или нулем!"
+      );
+      event.preventDefault();
+    }
+  });
+});
