22d5a783d82928e1a21cb5b4bc0a331f9ef564df
gmoroz <12345hasek@gmail.com>
2023-02-22 17:47:51 +0300

    feat: Validate team form fields before submitting
    This commit adds validation to the team form fields before submitting the form data to the server. The validation checks that the team name, country, city, coach name, points, and games count fields contain only letters and that the points and games count fields are positive integers or zero. If any of the fields fail validation, an error message is displayed, and the form is not submitted.

--- static/js/team.js
+++ static/js/team.js
@@ -3,6 +3,57 @@ $(document).ready(function () {
 
   $("#team-form").on("submit", function (event) {
     event.preventDefault();
+
+    // Validate the team name field
+    const teamNameField = $("#name");
+    const teamNameValue = teamNameField.val();
+    if (!/^[a-zA-Zа-яА-ЯёЁ\s]+$/.test(teamNameValue)) {
+      alert("В названии команды могут быть только буквы!");
+      return false;
+    }
+
+    // Validate the country field
+    const countryField = $("#country");
+    const countryValue = countryField.val();
+    if (!/^[a-zA-Zа-яА-ЯёЁ\s]+$/.test(countryValue)) {
+      alert("В названии страны могут быть только буквы!");
+      return false;
+    }
+
+    // Validate the city field
+    const cityField = $("#city");
+    const cityValue = cityField.val();
+    if (!/^[a-zA-Zа-яА-ЯёЁ\s]+$/.test(cityValue)) {
+      alert("В названии города могут быть только буквы!");
+      return false;
+    }
+
+    // Validate the coach field
+    const coachField = $("#coach");
+    const coachValue = coachField.val();
+    if (!/^[a-zA-Zа-яА-ЯёЁ\s]+$/.test(coachValue)) {
+      alert("В имени тренера могут быть только буквы!");
+      return false;
+    }
+
+    // Validate the points field
+    const pointsField = $("#points");
+    const pointsValue = pointsField.val();
+    if (pointsValue < 0) {
+      alert("Очки могут быть только положительными целыми числами или нулем!");
+      return false;
+    }
+
+    // Validate the games count field
+    const gamesCountField = $("#games-count");
+    const gamesCountValue = gamesCountField.val();
+    if (gamesCountValue < 0) {
+      alert(
+        "Количество игр может быть только положительным целым числом или нулем!"
+      );
+      return false;
+    }
+
     const formData = $(this).serialize();
 
     $.ajax({
