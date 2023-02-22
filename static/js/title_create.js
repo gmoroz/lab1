415933255d701dbb9c0350f8aba5423dc5e0b2f9
gmoroz <12345hasek@gmail.com>
2023-02-22 17:19:18 +0300

    feat: Add title creation functionality and associated script.
    A new JavaScript file, title_create.js, was added to provide functionality for creating titles. The file contains an event listener that handles the form submission and sends a POST request to the server. The response is logged to the console and appropriate actions are taken depending on the success or failure of the request.

---
+++ static/js/title_create.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  // Получаем форму и кнопку сохранения
+  const $titleForm = $("#title-form");
+  const $saveBtn = $(".save-btn");
+
+  // Обработчик события нажатия на кнопку сохранения
+  $saveBtn.on("click", function (e) {
+    e.preventDefault(); // Отменяем стандартное действие кнопки
+
+    // Получаем данные формы
+    const titleName = $("#name").val();
+
+    // Проверяем, что titleName состоит только из букв и пробелов
+    if (/^[a-zA-Zа-яА-ЯёЁ\s]+$/.test(titleName)) {
+      // Отправляем POST-запрос на сервер
+      $.ajax({
+        url: "/titles/",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": $titleForm
+            .find('input[name="csrfmiddlewaretoken"]')
+            .val(), // Получаем CSRF-токен из скрытого поля формы
+        },
+        data: JSON.stringify({ name: titleName }),
+        success: function (data) {
+          console.log("Data saved:", data);
+          // Дополнительные действия после успешного сохранения
+        },
+        error: function (xhr, status, error) {
+          console.error("Error saving data:", error);
+          // Дополнительные действия при ошибке сохранения
+        },
+      });
+    } else {
+      // Выводим сообщение об ошибке, если в названии титула содержатся недопустимые символы
+      alert("В названии титула могут быть только буквы!");
+    }
+  });
+});
