ddb45932464d018ebe9c62b8e687ea89bde8a8ed
gmoroz <12345hasek@gmail.com>
2023-02-22 20:17:43 +0300

    refactor: Modify title.js and title.html for better structure and UX
    In title.js:
    
    Replaced $saveBtn.on("click" with $titleForm.on("submit".
    Added comments for clarity.
    Replaced "method" with "type" for consistency.
    Removed "Content-Type" header as it is not necessary.
    Simplified data object to {name: titleName}.
    Added success and error messages for better UX.
    
    In title.html:
    
    Moved block.super after the custom stylesheet to avoid overriding.
    Changed permissions for editing the title.

--- static/js/title.js
+++ static/js/title.js
@@ -1,33 +1,45 @@
-$saveBtn.on("click", function (e) {
-  e.preventDefault(); // Отменяем стандартное действие кнопки
+$(document).ready(function () {
+  // Находим форму и кнопку отправки
+  const $titleForm = $("#title-form");
+  const $saveBtn = $(".save-btn");
 
-  // Получаем данные формы
-  const titleId = $("#title-id").val();
-  const titleName = $("#name").val();
+  // Добавляем обработчик на отправку формы
+  $titleForm.on("submit", function (event) {
+    event.preventDefault(); // Отменяем стандартное поведение формы
 
-  // Проверяем наличие только букв в названии титула
-  const regex = /^[a-zA-Zа-яА-ЯёЁ\s]+$/;
-  if (!regex.test(titleName)) {
-    alert("В названии титула могут быть только буквы!");
-    return; // Остановить выполнение функции
-  }
+    // Получаем данные формы
+    const titleId = $("#title-id").val();
+    const titleName = $("#name").val();
 
-  // Отправляем PUT-запрос на сервер
-  $.ajax({
-    url: `/titles/${titleId}/`,
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      "X-CSRFToken": $titleForm.find('input[name="csrfmiddlewaretoken"]').val(), // Получаем CSRF-токен из скрытого поля формы
-    },
-    data: JSON.stringify({ name: titleName }),
-    success: function (data) {
-      console.log("Data saved:", data);
-      // Дополнительные действия после успешного сохранения
-    },
-    error: function (xhr, status, error) {
-      console.error("Error saving data:", error);
-      // Дополнительные действия при ошибке сохранения
-    },
+    const regex = /^[a-zA-Zа-яА-ЯёЁ\s]+$/;
+    if (!regex.test(titleName)) {
+      alert("В названии титула могут быть только буквы!");
+      return; // Остановить выполнение функции
+    }
+    // Отправляем PUT-запрос на сервер
+    $.ajax({
+      url: `/titles/${titleId}/`,
+      type: "PUT",
+      headers: {
+        "X-CSRFToken": $titleForm
+          .find('input[name="csrfmiddlewaretoken"]')
+          .val(), // Получаем CSRF-токен из скрытого поля формы
+      },
+      data: {
+        name: titleName,
+      },
+      success: function (data) {
+        console.log("Данные успешно сохранены:", data);
+        // Выводим сообщение об успешном сохранении
+        alert("Данные успешно сохранены!");
+        // Редирект на страницу со списком титулов
+        window.location.href = "/titles/";
+      },
+      error: function (xhr, status, error) {
+        console.error("Ошибка при сохранении данных:", error);
+        // Выводим сообщение об ошибке
+        alert("Ошибка при сохранении данных!");
+      },
+    });
   });
 });
