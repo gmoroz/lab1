111393a7f49562e2a3b76deff781640819c84797
gmoroz <12345hasek@gmail.com>
2023-02-22 16:54:01 +0300

    feat: Add title name validation on the front-end
    The previous version of the code only sent a PUT request to the server to update the title name, without any client-side validation. This update adds a regular expression check to ensure that the title name only contains letters, both Latin and Cyrillic. If the validation fails, an alert message is shown and the request is not sent to the server.

--- static/js/title.js
+++ static/js/title.js
@@ -1,35 +1,33 @@
-$(document).ready(function () {
-  // Получаем форму и кнопку сохранения
-  const $titleForm = $("#title-form");
-  const $saveBtn = $(".save-btn");
+$saveBtn.on("click", function (e) {
+  e.preventDefault(); // Отменяем стандартное действие кнопки
 
-  // Обработчик события нажатия на кнопку сохранения
-  $saveBtn.on("click", function (e) {
-    e.preventDefault(); // Отменяем стандартное действие кнопки
+  // Получаем данные формы
+  const titleId = $("#title-id").val();
+  const titleName = $("#name").val();
 
-    // Получаем данные формы
-    const titleId = $("#title-id").val();
-    const titleName = $("#name").val();
+  // Проверяем наличие только букв в названии титула
+  const regex = /^[a-zA-Zа-яА-ЯёЁ\s]+$/;
+  if (!regex.test(titleName)) {
+    alert("В названии титула могут быть только буквы!");
+    return; // Остановить выполнение функции
+  }
 
-    // Отправляем PUT-запрос на сервер
-    $.ajax({
-      url: `/titles/${titleId}/`,
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRFToken": $titleForm
-          .find('input[name="csrfmiddlewaretoken"]')
-          .val(), // Получаем CSRF-токен из скрытого поля формы
-      },
-      data: JSON.stringify({ name: titleName }),
-      success: function (data) {
-        console.log("Data saved:", data);
-        // Дополнительные действия после успешного сохранения
-      },
-      error: function (xhr, status, error) {
-        console.error("Error saving data:", error);
-        // Дополнительные действия при ошибке сохранения
-      },
-    });
+  // Отправляем PUT-запрос на сервер
+  $.ajax({
+    url: `/titles/${titleId}/`,
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": $titleForm.find('input[name="csrfmiddlewaretoken"]').val(), // Получаем CSRF-токен из скрытого поля формы
+    },
+    data: JSON.stringify({ name: titleName }),
+    success: function (data) {
+      console.log("Data saved:", data);
+      // Дополнительные действия после успешного сохранения
+    },
+    error: function (xhr, status, error) {
+      console.error("Error saving data:", error);
+      // Дополнительные действия при ошибке сохранения
+    },
   });
 });
