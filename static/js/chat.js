0767c509c185c1adaea55b5b607d966d71a3d2e6
gmoroz <12345hasek@gmail.com>
2023-02-23 17:30:26 +0300

    refactor: Update chat.js and chat.html to improve code structure and functionality
    This commit improves the chat functionality by refactoring the code in chat.js to improve readability and functionality. The changes include using a shorter equivalent to jQuery's "ready" function, changing the way to get the room name from the URL, creating a function to handle sending messages via WebSocket, and adding event handlers for the send button and the "enter" key. The code has also been updated to use the "get" shorthand method instead of "ajax" to load messages from the server. Additionally, a function has been added to scroll the messages container to the bottom, and jQuery is used to create message elements instead of concatenating strings.
    
    In chat.html, a CSRF token has been added to the form to prevent cross-site request forgery.

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,50 +1,73 @@
-$(document).ready(function () {
-  var url = window.location.href;
-  var room_name = url.match(/\/chat\/(.*)\//)[1];
-  const username = $("#username").val();
-  const messages = $("#messages");
+$(function () {
+  // Get the room name from the URL
+  var room_name = window.location.pathname.split("/")[2];
 
-  // Функция для получения списка сообщений по AJAX-запросу
-  function getMessages() {
-    $.ajax({
-      url: `/chats/${room_name}/`,
-      type: "GET",
-      success: function (response) {
-        messages.empty();
-        response.forEach((message) => {
-          messages.append(`<li>${message.user}: ${message.text}</li>`);
-        });
-      },
-    });
-  }
+  // Get the username
+  var username = $("#username").val();
 
-  // Получаем список сообщений при загрузке страницы
-  getMessages();
-
-  // Обработчик события отправки нового сообщения
-  $("#send-button").click(function () {
-    const text = $("#message-input").val();
-    $.ajax({
-      url: `/chats/${room_name}/`,
-      type: "PUT",
-      data: {
-        text: text,
-        username: username,
-      },
-      success: function () {
-        $("#message-input").val("");
-        getMessages();
-      },
-    });
-  });
-
-  // WebSocket-соединение для получения новых сообщений
-  const chatSocket = new WebSocket(
-    "ws://" + window.location.host + `/ws/chat/${room_name}/`
+  // Connect to the WebSocket
+  var socket = new WebSocket(
+    "ws://" + window.location.host + "/ws/chat/" + room_name + "/"
   );
 
-  chatSocket.onmessage = function (event) {
-    const message = JSON.parse(event.data);
-    messages.append(`<li>${message.username}: ${message.text}</li>`);
+  // Function for scrolling to the bottom of the messages container
+  function scrollMessagesContainer() {
+    var messagesContainer = $(".messages-container");
+    messagesContainer.scrollTop(messagesContainer.prop("scrollHeight"));
+  }
+
+  // Handle incoming WebSocket messages
+  socket.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+    var message = $("<li>");
+    var usernameSpan = $("<span>")
+      .addClass("username")
+      .text(data.username + ": ");
+    var textSpan = $("<span>").addClass("text").text(data.text);
+    message.append(usernameSpan);
+    message.append(textSpan);
+    $("#messages").append(message);
+    scrollMessagesContainer();
   };
+
+  // Send a message to the server
+  function sendMessage() {
+    var messageInput = $("#message-input");
+    var messageText = messageInput.val().trim();
+    if (messageText) {
+      var data = {
+        text: messageText,
+      };
+      socket.send(JSON.stringify(data));
+      messageInput.val("");
+    }
+  }
+
+  // Handle sending a message when the send button is clicked
+  $("#send-button").click(function () {
+    sendMessage();
+  });
+
+  // Handle sending a message when the enter key is pressed
+  $("#message-input").keypress(function (event) {
+    if (event.keyCode == 13) {
+      sendMessage();
+      event.preventDefault();
+    }
+  });
+
+  // Load initial messages from the server
+  $.get("/chats/" + room_name + "/", function (data) {
+    for (var i = 0; i < data.length; i++) {
+      var message = $("<li>");
+      var usernameSpan = $("<span>")
+        .addClass("username")
+        .text(data[i].username + ": ");
+      var textSpan = $("<span>").addClass("text").text(data[i].text);
+      message.append(usernameSpan);
+      message.append(textSpan);
+      $("#messages").append(message);
+    }
+    scrollMessagesContainer();
+  });
 });
